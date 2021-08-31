59ba4f7e9bcf3befcd6b05900e94adb9b7c096c2
Amy Tran <amyaitran@gmail.com>
2021-08-31 11:22:34 -0700

    added a toggle variable in js

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -2,26 +2,32 @@
 // create a JS storage for a reference to the HTML body background.
 
 // create a click event listener that will perform actions when the button is clicked.
+// create a switch for the light bulb, with the default value of "off".
 
 // define what will happen when the button is clicked:
-//   -if the button was previously set to dark, then
-//     -set the button to light mode
-//     -set the background to light mode
-//   -otherwise, if the button was previously set to light, then
+//   -if the switch is set to off, then
+//     -turn the switch "on"
 //     -set the button to dark mode
 //     -set the background to dark mode
+//   -otherwise, if the switch is set to "on", then
+//     -turn the switch "off"
+//     -set the button to light mode
+//     -set the background to light mode
 
 var $button = document.querySelector('button');
 var $background = document.querySelector('body');
 
 $button.addEventListener('click', handleClick);
+var buttonOn = false;
 
 function handleClick(event) {
-  if ($button.className === 'bt-dark') {
-    $button.className = 'bt-light';
-    $background.className = 'bg-light';
-  } else if ($button.className === 'bt-light') {
+  if (buttonOn === false) {
+    buttonOn = true;
     $button.className = 'bt-dark';
     $background.className = 'bg-dark';
+  } else if (buttonOn === true) {
+    buttonOn = false;
+    $button.className = 'bt-light';
+    $background.className = 'bg-light';
   }
 }
