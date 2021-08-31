0d8d19c77672c670b653368a321782022e2f59c3
Amy Tran <amyaitran@gmail.com>
2021-08-30 17:31:06 -0700

    edits to simplify code

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,13 +1,7 @@
 /* exported getValue */
 
-// look at the listed traits of the object, and the values of those traits.
-// if the trait that we're looking at is the same as the key we're looking for,
-// then return the value of that trait.
+// return the value of the object at its trait "key"
 
 function getValue(object, key) {
-  for (var trait in object) {
-    if (trait === key) {
-      return object[trait];
-    }
-  }
+  return object[key];
 }
--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,11 +1,7 @@
 /* exported setValue */
 
-// if the trait "key" is not a part of the object,
-// then add the name of the trait into the object
-// & assign the value of that trait to the object
+// set the trait "key" into the object and assign to it the value of that trait
 
 function setValue(object, key, value) {
-  if (object.key === undefined) {
-    object[key] = value;
-  }
+  object[key] = value;
 }
