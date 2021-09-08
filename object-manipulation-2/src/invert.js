b0ea7024d3551c517ec805a654caf5c547cdb3a5
Amy Tran <amyaitran@gmail.com>
2021-09-07 21:25:49 -0700

    changed for loop into for-in loop

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,18 +1,14 @@
 /* exported invert */
 
 // create a storage for the output.
-// extract and create a storage for all the traits from the source.
-// extract and create a separate storage for all the values from the source.
-// for each trait and value from the source,
-// assign the source value as the output trait, and the source trait as the output value.
+// look at all the properties in "source".
+// set the value of each of "source's" properties as a property of the output, and set the value as the source property.
 // return the output.
 
 function invert(source) {
   var output = {};
-  var sourceKeys = Object.keys(source);
-  var sourceValues = Object.values(source);
-  for (var i = 0; i < sourceKeys.length; i++) {
-    output[sourceValues[i]] = sourceKeys[i];
+  for (var props in source) {
+    output[source[props]] = props;
   }
   return output;
 }
