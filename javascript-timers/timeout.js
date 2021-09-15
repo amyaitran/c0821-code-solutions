cf0237273d32f4d3b779dd7fa91386d17feffff6
Amy Tran <amyaitran@gmail.com>
2021-09-15 11:13:46 -0700

    deleted extra code

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -1,10 +1,4 @@
 var $h1 = document.querySelector('h1');
 
-// setTimeout(changeText($h1), 2000);
-
-// function changeText(text) {
-//   text.textContent = 'Hello There';
-// }
-
 setTimeout(function () { $h1.textContent = 'Hello There'; }, 2000)
 ;
