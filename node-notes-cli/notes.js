516d4bdc65be37fd84f6d11d3377a6d4680d7f32
Amy Tran <amyaitran@gmail.com>
2021-10-07 11:46:09 -0700

    allow user to add notes

--- node-notes-cli/notes.js
+++ node-notes-cli/notes.js
@@ -1,37 +1,21 @@
 const fs = require('fs');
+const JSONdata = require('./data.json');
+
 console.log(process.argv);
 
 if (process.argv[2] === 'read') {
-  fs.readFile('data.json', (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(JSON.parse(data));
-  });
+  console.log(JSONdata);
 }
 
 if (process.argv[2] === 'create') {
 
-  const newNote = {
-    nextId: 5,
-    notes: {
-      1: 'The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.',
-      2: 'Prototypal inheritance is how JavaScript objects delegate behavior.',
-      3: 'In JavaScript, the value of `this` is determined when a function is called; not when it is defined.',
-      4: 'A closure is formed when a function retains access to variables in its lexical scope.'
-    }
-  };
+  Object.assign(JSONdata.notes, { [JSONdata.nextId]: process.argv[3] });
+  JSONdata.nextId++;
 
-  // const theNextId = newNote.nextId;
-  Object.assign(newNote.notes, { [newNote.nextId]: process.argv[3] });
-  newNote.nextId++;
-
-  const data = JSON.stringify(newNote, null, 2);
-  console.log('making a new note lol');
-  fs.writeFile('data.json', data, err => {
+  const stringifyData = JSON.stringify(JSONdata, null, 2);
+  fs.writeFile('data.json', stringifyData, err => {
     if (err) {
       console.error(err);
     }
-    console.log('data written??');
   });
 }
