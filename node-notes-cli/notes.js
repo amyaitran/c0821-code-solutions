4af41e381012cb48529cc55f2e4cb304ca804511
amyaitran <amyaitran@gmail.com>
2021-10-07 15:55:27 -0700

    completed node-notes-cli exercise

--- node-notes-cli/notes.js
+++ node-notes-cli/notes.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const JSONdata = require('./data.json');
 
-console.log(process.argv);
-
 if (process.argv[2] === 'read') {
   console.log(JSONdata);
 }
@@ -22,6 +20,7 @@ if (process.argv[2] === 'create') {
 if (process.argv[2] === 'update') {
   const notesId = process.argv[3];
   const notesUpdate = process.argv[4];
+
   JSONdata.notes[notesId] = notesUpdate;
 
   const stringifyData = JSON.stringify(JSONdata, null, 2);
@@ -32,12 +31,15 @@ if (process.argv[2] === 'update') {
   });
 }
 
-// delete 1
 if (process.argv[2] === 'delete') {
-  for (const keys in JSONdata) {
-    console.log(JSONdata[keys]);
-    if (JSONdata[keys] === process.argv[3]) {
-      delete JSONdata.keys;
+  const keyToDelete = process.argv[3];
+
+  delete JSONdata.notes[keyToDelete];
+
+  const stringifyData = JSON.stringify(JSONdata, null, 2);
+  fs.writeFile('data.json', stringifyData, err => {
+    if (err) {
+      console.error(err);
     }
-  }
+  });
 }
