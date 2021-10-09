a56a784a0126511d90a2afc35a5bf9e564ab88d9
Amy Tran <amyaitran@gmail.com>
2021-10-09 11:44:37 -0700

    client can post a new note

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const middleware = express.json();
 const data = require('./data.json');
 
 app.listen(3000, () => {
@@ -7,7 +8,9 @@ app.listen(3000, () => {
   console.log('Express server listening on port 3000');
 });
 
-app.get('/api/notes', function (req, res) {
+app.use(middleware);
+
+app.get('/api/notes', (req, res) => {
   const arr = [];
   for (const keys in data.notes) {
     arr.push(data.notes[keys]);
@@ -15,12 +18,25 @@ app.get('/api/notes', function (req, res) {
   res.status(200).json(arr);
 });
 
-app.get('/api/notes/:id', function (req, res) {
+app.get('/api/notes/:id', (req, res) => {
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!data.notes[req.params.id]) {
     res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
   } else {
     res.status(200).json(data.notes[req.params.id]);
+  }
+});
+
+app.post('/api/notes', (req, res, error) => {
+  if (!req.body) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (req.body) {
+    req.body.id = data.nextId;
+    res.status(201).send(req.body);
+    data.notes[data.nextId] = req.body;
+    data.nextId++;
+  } else {
+    res.status(500).json({ error: 'An unexpected error occurred.' });
   }
 });
