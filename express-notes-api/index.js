b8d6a806274395c2822f64014834ec1c86efec4f
amyaitran <amyaitran@gmail.com>
2021-10-10 09:09:32 -0700

    modify code to allow file changes in data.json

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const middleware = express.json();
 const data = require('./data.json');
+const fs = require('fs');
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -32,10 +33,16 @@ app.post('/api/notes', (req, res) => {
   if (!req.body) {
     res.status(400).json({ error: 'content is a required field' });
   } else if (req.body) {
-    req.body.id = data.nextId;
-    res.status(201).send(req.body);
-    data.notes[data.nextId] = req.body;
+    Object.assign(data.notes, { [data.nextId]: req.body });
+    data.notes[data.nextId].id = data.nextId;
     data.nextId++;
+    const stringifyData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', stringifyData, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    res.status(201).send(req.body);
   } else {
     res.status(500).json({ error: 'An unexpected error occurred.' });
   }
@@ -48,6 +55,12 @@ app.delete('/api/notes/:id', (req, res) => {
     res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
   } else if (data.notes[req.params.id]) {
     delete data.notes[req.params.id];
+    const stringifyData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', stringifyData, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
     res.sendStatus(204);
   } else {
     res.status(500).json({ error: 'An unexpected error occurred.' });
@@ -64,6 +77,12 @@ app.put('/api/notes/:id', (req, res) => {
   } else if (data.notes[req.params.id]) {
     req.body.id = parseInt(req.params.id);
     data.notes[req.params.id] = req.body;
+    const stringifyData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', stringifyData, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
     res.status(200).json(data.notes[req.params.id]);
   } else {
     res.status(500).json({ error: 'An unexpected error occurred.' });
