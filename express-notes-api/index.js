c07bf5566f8756b6fe337debf0cc9f569b0b95a6
amyaitran <amyaitran@gmail.com>
2021-10-10 09:15:10 -0700

    status code 500 appears upon fs.writeFile error

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -32,19 +32,17 @@ app.get('/api/notes/:id', (req, res) => {
 app.post('/api/notes', (req, res) => {
   if (!req.body) {
     res.status(400).json({ error: 'content is a required field' });
-  } else if (req.body) {
+  } else {
     Object.assign(data.notes, { [data.nextId]: req.body });
     data.notes[data.nextId].id = data.nextId;
     data.nextId++;
     const stringifyData = JSON.stringify(data, null, 2);
     fs.writeFile('data.json', stringifyData, err => {
       if (err) {
-        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
       }
     });
     res.status(201).send(req.body);
-  } else {
-    res.status(500).json({ error: 'An unexpected error occurred.' });
   }
 });
 
@@ -53,17 +51,15 @@ app.delete('/api/notes/:id', (req, res) => {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!data.notes[req.params.id]) {
     res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
-  } else if (data.notes[req.params.id]) {
+  } else {
     delete data.notes[req.params.id];
     const stringifyData = JSON.stringify(data, null, 2);
     fs.writeFile('data.json', stringifyData, err => {
       if (err) {
-        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
       }
     });
     res.sendStatus(204);
-  } else {
-    res.status(500).json({ error: 'An unexpected error occurred.' });
   }
 });
 
@@ -74,18 +70,16 @@ app.put('/api/notes/:id', (req, res) => {
     res.status(400).json({ error: 'content is a required field' });
   } else if (!data.notes[req.params.id]) {
     res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
-  } else if (data.notes[req.params.id]) {
+  } else {
     req.body.id = parseInt(req.params.id);
     data.notes[req.params.id] = req.body;
     const stringifyData = JSON.stringify(data, null, 2);
     fs.writeFile('data.json', stringifyData, err => {
       if (err) {
-        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
       }
     });
     res.status(200).json(data.notes[req.params.id]);
-  } else {
-    res.status(500).json({ error: 'An unexpected error occurred.' });
   }
 })
 ;
