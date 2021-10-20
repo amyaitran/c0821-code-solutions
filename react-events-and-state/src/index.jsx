64f903c4ab9ed7ac25003edf8158a7b4c6aa7c27
Amy Tran <amyaitran@gmail.com>
2021-10-20 07:13:45 -0700

    completed react-events-and-state exercise

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isClicked: false };
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    const isClicked = this.state.isClicked;
+    let button;
+    if (isClicked) {
+      return (
+      <button onClick={this.handleClick}>
+        thanks!
+      </button>);
+    } else {
+      return (
+      <button onClick={this.handleClick}>
+        Click Me!
+      </button>);
+    }
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.querySelector('#root')
+);
